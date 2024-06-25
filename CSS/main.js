290a7f80edd11c6cbf38aedd5ee67f4a2e7fe347
Yulduz <yulduz.karshiyeva@mail.ru>
2024-06-25 11:00:45 +0500

    index.html and main.css add

---
+++ CSS/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 5,
+        margin: 10,
+        loop: true,
+        center: true,
+        touchDrag:true
+    });
+    
+  });
